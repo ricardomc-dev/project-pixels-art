8d507e9f9c6a35ddc3c1a023d632fd611b13df8e
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-09-10 15:57:10 -0300

    Resolvido Itens 2 e 3

---
+++ script.js
@@ -0,0 +1,13 @@
+/* 
+let baseSquare = 5
+
+function addColorBox(){
+    let createLine = document.createElement('div')
+    createLine.innerHTML = 'teste';
+    let getElementBox = document.querySelectorAll('.color');
+    for (let index = 0; index <= getElementBox.length; index += 1) {
+        getElementBox[index].appendChild(createLine);
+    }
+}
+addColorBox();
+*/
