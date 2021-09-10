5ea8797a546129681e9c122fc461d47f0af25561
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-09-10 18:16:40 -0300

    Resolvido Itens 4 e 5

--- script.js
+++ script.js
@@ -1,13 +1,62 @@
-/* 
-let baseSquare = 5
-
-function addColorBox(){
-    let createLine = document.createElement('div')
-    createLine.innerHTML = 'teste';
-    let getElementBox = document.querySelectorAll('.color');
-    for (let index = 0; index <= getElementBox.length; index += 1) {
-        getElementBox[index].appendChild(createLine);
+/*
+let getMain = document.getElementsByTagName('main')[0];
+let board = document.createElement('div');
+board.id = 'pixel-board';
+getMain.appendChild(board);
+*/
+/*
+function createLines() {
+    let numberOfLines = 5;
+    let boxLines = document.createElement('div');
+    let pixelBoard = document.querySelector('#pixel-board')
+    for (let index = 1; index <= numberOfLines; index += 1){
+        pixelBoard.appendChild(boxLines); 
     }
 }
-addColorBox();
+createLines();
 */
+/*
+function createBox(){
+    let box = document.createElement('div');
+    box.classList.add('pixel');
+    return box;
+}
+let baseSquare = 5;
+*/
+/*
+function createColumn() {
+    let numberOfColumn = 5;
+    let box = document.createElement('div');
+    let getLines = document.querySelectorAll('.line');
+    console.log(getLines)
+    for (let index = 0; index < getLines.length; index += 1) {
+        getLines[index].appendChild(box)
+    }
+}
+createColumn()
+*/
+
+window.onload = function() {
+    let baseSquare = 5;
+    let lines = document.querySelectorAll(".line");
+  
+    function createBox(className) {
+      let box = document.createElement("div");
+      box.className = className;
+      return box;
+    }
+  
+    function fillLine(divLine) {
+      for (let lineColumn = 1; lineColumn <= baseSquare; lineColumn += 1) {
+          let box = createBox("pixel");
+          divLine.appendChild(box);
+      }
+    }
+    
+    function fillSquare(lines) {
+        for(let index = 0; index < lines.length; index += 1) {
+          fillLine(lines[index]);
+        }
+    }
+    fillSquare(lines);
+}
