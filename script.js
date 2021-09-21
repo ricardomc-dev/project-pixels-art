512fffc4c883a0f6dea3677c76e60e38746d1d5e
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-09-21 10:54:31 -0300

    Correção parcial do linter 3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+const pixelBoard = '#pixel-board';
+let n = 5;
 function setColorPalettes() {
   const boxPalettes = document.querySelectorAll('.color');
   const colorArray = ['black', '#F01E59', '#181695', '#20951A'];
@@ -24,11 +25,11 @@ function setClassPalettes() {
     palettesBox[index].addEventListener('click', changeClassSelected);
   }
 }
-setClassPalettes();    
+setClassPalettes();
 
-function createPixel(number) {
-  const baseSquare = number;
-  const board = document.querySelector('#pixel-board');
+function createPixel() {
+  const baseSquare = n;
+  const board = document.querySelector(pixelBoard);
   for (let index = 1; index <= baseSquare; index += 1) {
     const lines = document.createElement('div');
     lines.classList.add('line');
@@ -40,48 +41,50 @@ function createPixel(number) {
     }
   }
 }
-createPixel(5);
+createPixel();
 
+function changeBackgroundColor(event) {
+  const colorSelected = document.querySelector('.selected');
+  const color = colorSelected.style.backgroundColor;
+  const element = event.target;
+  console.log(color);
+  element.style.backgroundColor = color;
+}
 function changePixelColor() {
   const pixel = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].addEventListener('click', function(event) {
-      const colorSelected = document.querySelector('.selected');
-      const color = colorSelected.style.backgroundColor;
-      console.log(color);
-      event.target.style.backgroundColor = color;
-    })
+    pixel[index].addEventListener('click', changeBackgroundColor);
   }
 }
 changePixelColor();
-
+function clearPixel() {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
+  }
+}
 function clearBoard() {
   const buttonSection = document.createElement('section');
   buttonSection.id = 'button-section';
   const getMain = document.getElementsByTagName('main')[0];
-  const getBoard = document.querySelector('#pixel-board');
+  const getBoard = document.querySelector(pixelBoard);
   getMain.insertBefore(buttonSection, getBoard);
   const button = document.createElement('button');
   button.id = 'clear-board';
   button.innerHTML = 'Limpar';
   buttonSection.appendChild(button);
-  const pixel = document.querySelectorAll('.pixel');
-  button.addEventListener('click', function() {
-    for (let index = 0; index < pixel.length; index += 1) {
-      pixel[index].style.backgroundColor = 'white';
-    }
-  });
+  button.addEventListener('click', clearPixel);
 }
 clearBoard();
 
 function createSizeButton() {
   const getMain = document.getElementsByTagName('main')[0];
-  const getBoard = document.querySelector('#pixel-board'); 
+  const getBoard = document.querySelector(pixelBoard);
   const sizeSection = document.createElement('section');
   sizeSection.id = 'size-section';
-  getMain.insertBefore(sizeSection, getBoard)
+  getMain.insertBefore(sizeSection, getBoard);
   const sizeInput = document.createElement('input');
-  const n = sizeInput.value;
+  n = sizeInput.value;
   sizeInput.id = 'board-size';
   sizeInput.min = 5;
   sizeInput.max = 50;
@@ -91,6 +94,6 @@ function createSizeButton() {
   sizeButton.innerHTML = 'VQV';
   sizeSection.appendChild(sizeInput);
   sizeSection.appendChild(sizeButton);
-  sizeButton.addEventListener('click', createPixel(n));
+  sizeButton.addEventListener('click', createPixel);
 }
 createSizeButton();
